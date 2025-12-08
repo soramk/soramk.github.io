624353654e19d23eb4db412067f05c09cf023e44
soramk <sora0330@gmail.com>
2025-12-08 23:56:18 +0900

    Update business.js

--- data/business.js
+++ data/business.js
@@ -1,4 +1,4 @@
 window.dataset_business = [
   { "l": { "w":"Collect", "b":[{"p":"k","t":"velar"},{"p":"o","t":"mid"},{"p":"l","t":"l_shape"},{"p":"eh","t":"mid"},{"p":"k","t":"velar"},{"p":"t","t":"alveolar"}] }, "r": { "w":"Correct", "b":[{"p":"k","t":"velar"},{"p":"o","t":"mid"},{"p":"r","t":"r_shape"},{"p":"eh","t":"mid"},{"p":"k","t":"velar"},{"p":"t","t":"alveolar"}] } },
   { "l": { "w":"Leader", "b":[{"p":"l","t":"l_shape"},{"p":"iy","t":"spread"},{"p":"d","t":"alveolar"},{"p":"er","t":"r_shape"}] }, "r": { "w":"Reader", "b":[{"p":"r","t":"r_shape"},{"p":"iy","t":"spread"},{"p":"d","t":"alveolar"},{"p":"er","t":"r_shape"}] } }
-]
+];
