cd690290684645cbb7b50f5ebe54414f5517c4d5
soramk <sora0330@gmail.com>
2025-12-08 23:56:08 +0900

    Update advanced.js

--- data/advanced.js
+++ data/advanced.js
@@ -1,4 +1,4 @@
-[
+window.dataset_advanced = [
   { "l": { "w":"Alive", "b":[{"p":"a","t":"mid"},{"p":"l","t":"l_shape"},{"p":"ay","t":"wide"},{"p":"v","t":"labiodental"}] }, "r": { "w":"Arrive", "b":[{"p":"a","t":"mid"},{"p":"rr","t":"r_shape"},{"p":"ay","t":"wide"},{"p":"v","t":"labiodental"}] } },
   { "l": { "w":"Tile", "b":[{"p":"t","t":"alveolar"},{"p":"ay","t":"wide"},{"p":"l","t":"l_shape"}] }, "r": { "w":"Tire", "b":[{"p":"t","t":"alveolar"},{"p":"ay","t":"wide"},{"p":"er","t":"r_shape"}] } }
-]
+];
